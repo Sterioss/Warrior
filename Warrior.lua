3414bc14c2a1474be1ce7be65a95f8a1c5a66d03
Sterioss <sterios@live.fr>
2017-11-26 11:26:57 +0100

    Includes talents check for the rotation

--- Warrior.lua
+++ Warrior.lua
@@ -18,12 +18,14 @@ local function Combat()
   if UnitCanAttack('player', target) and player.inmelee and
    target.infront(target,180) and target.alive
   then
-    
+
     -- If we have the head or the 4pc and we're bursting - cast BS
-    if player.buff(BattleCry).up and
-    (T20parts >= 4 or itemequipped(TheGreatStormsEye))
-    and castable(Bladestorm,target) then
-      return cast(Bladestorm,target)
+    if not player.talent(7,3) then
+      if player.buff(BattleCry).up and
+      (T20parts >= 4 or itemequipped(TheGreatStormsEye))
+      and castable(Bladestorm,target) then
+        return cast(Bladestorm,target)
+      end
     end
 
     -- If we don't have the Shattered buff - cast CS
@@ -33,18 +35,24 @@ local function Combat()
     end
 
     -- If rend remaining time is under 2.4 or we're about to burst - cast rend
-    if (target.debuff(rend).duration <= player.GCD or
-    (target.debuff(rend).duration < 5 and
-    player.spell(BattleCry).cooldown < 2 and
-    (player.spell(Bladestorm).cooldown < 2 or T20parts >= 2)))
-    and castable(Rend,target)
-    then
-      return cast(Rend,target)
+    if player.talent(3,2) then
+      if (target.debuff(rend).duration <= player.GCD or
+      (target.debuff(rend).duration < 5 and
+      player.spell(BattleCry).cooldown < 2 and
+      (player.spell(Bladestorm).cooldown < 2 or T20parts >= 2)))
+      and castable(Rend,target)
+      then
+        return cast(Rend,target)
+      end
     end
 
     -- If overpower is up and we're not bursting - cast overpower
-    if player.buff(Overpower).up and player.buff(BattleCry).down
-    and castable(Overpower,target)
-    then
-      return cast(Overpower,target)
+    if player.talent(1,2) then
+      if player.buff(Overpower).up and player.buff(BattleCry).down
+      and castable(Overpower,target)
+      then
+        return cast(Overpower,target)
+      end
     end
+
+    
